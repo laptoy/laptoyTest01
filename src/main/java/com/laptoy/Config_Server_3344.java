435ed1d2f9fe6c2e2d17659f8a4676d36fe2430e
laptoy <laptoy458@163.com>
2021-09-05 15:49:51 +0800

    First

---
+++ src/main/java/com/laptoy/Config_Server_3344.java
@@ -0,0 +1,17 @@
+package com.laptoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author： 黄乐
+ * @Date： 2021/9/5 10:28
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_Server_3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(Config_Server_3344.class, args);
+    }
+}
